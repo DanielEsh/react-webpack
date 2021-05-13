31d3bf8a7465fc95d42117a7c8429a4a12a839ec
Daniel <daniilterawars@gmail.com>
2021-05-13 20:01:40 +0300

    feat(linter): add esline and prettier

--- src/example.ts
+++ src/example.ts
@@ -1,34 +1,33 @@
-
+/* eslint-disable */
 
 const concat = (first: string, last: string): string => {
-    return first + last;
-}
+  return first + last;
+};
 
 console.log(concat('Hello ', 'World')); // -> Hello World;
 
 interface myHomeTaskProps {
-    howIDoIt: string;
-    someArray: (number | string)[];
-    withData: Object[];
+  howIDoIt: string;
+  someArray: (number | string)[];
+  withData: Object[];
 }
 
 const myHomeTask: myHomeTaskProps = {
-    howIDoIt: "I Do It Wel",
-    someArray: ["string one", "string two", 42],
-    withData: [{ howIDoIt: "I Do It Wel", someArray: ["string one", 23] }],
-}
+  howIDoIt: 'I Do It Wel',
+  someArray: ['string one', 'string two', 42],
+  withData: [{ howIDoIt: 'I Do It Wel', someArray: ['string one', 23] }],
+};
 
 interface MyArray<T> {
-    [N: number]: T;
+  [N: number]: T;
 
-    reduce(callback: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
+  reduce(callback: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T): T;
 }
 
-const arr: MyArray<number> = [1,2,3,4,5];
+const arr: MyArray<number> = [1, 2, 3, 4, 5];
 
-const sum = arr.reduce((previousValue,currentValue) => {
-    return previousValue + currentValue;
-})
+const sum = arr.reduce((previousValue, currentValue) => {
+  return previousValue + currentValue;
+});
 
 console.log(sum); // -> 15
-
