cf960df8dd01497e2081b6412fc78e19bedb4d77
Daniel <daniilterawars@gmail.com>
2021-05-16 16:34:41 +0300

    feat(button): add button component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import Header from '../../components/Header';
 
 import s from './Home.module.scss';
+import Button from '../../components/Button';
 
 const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
+      <div>
+        <Button>Test</Button>
+      </div>
     </div>
   );
 };
