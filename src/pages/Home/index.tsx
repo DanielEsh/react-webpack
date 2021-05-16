1b3f79e5ce9a4c4a77cd6e12c58b3eae2ef3fc8d
Daniel <daniilterawars@gmail.com>
2021-05-16 16:42:25 +0300

    feat(fonts): add fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,6 +10,9 @@ const HomePage = () => {
     <div className={s.root}>
       <Header />
       <div>
+        <h1>Find all your favorite Pokemon</h1>
+
+        <h3>You can know the type of Pokemon, its strengths, disadvantages and abilities</h3>
         <Button>Test</Button>
       </div>
     </div>
