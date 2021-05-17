fb35db711020ba3d6c02bb7f6d334969be1d3e60
Daniel <daniilterawars@gmail.com>
2021-05-17 08:48:45 +0300

    feat(HomePage): add Layout

--- src/components/Parallax/index.tsx
+++ src/components/Parallax/index.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react';
 import s from './Parallax.module.scss';
 
 import SmallPoketballPng from './assets/PokeBall1.png';
-import CloudsPng from './assets/Clouds.png';
+import Cloud1Png from './assets/Cloud1.png';
+import Cloud2Png from './assets/Cloud2.png';
 import Poketball from './assets/Pokeball2.png';
 import PikachuPng from './assets/Pikachu.png';
 
@@ -26,28 +27,39 @@ const Parallax = () => {
   return (
     <div className={s.root}>
       <div
-        style={{
-          transform: `translate(${screenX * 0.04}px, ${screenY * 0.04}px)`,
-        }}>
-        <img src={SmallPoketballPng} alt="" />
-      </div>
-      <div
-        style={{
-          transform: `translate(${screenX * 0.03}px, ${screenY * 0.03}px)`,
-        }}>
-        <img src={CloudsPng} alt="" />
-      </div>
-      <div
-        style={{
-          transform: `translate(${screenX * 0.02}px, ${screenY * 0.02}px)`,
-        }}>
-        <img src={Poketball} alt="" />
-      </div>
-      <div
+        className={s.smallPokeBall}
         style={{
           transform: `translate(${screenX * 0.01}px, ${screenY * 0.01}px)`,
         }}>
-        <img src={PikachuPng} alt="" />
+        <img src={SmallPoketballPng} alt="Small PokeBall" />
+      </div>
+      <div
+        className={s.cloud}
+        style={{
+          transform: `translate(${screenX * 0.02}px, ${screenY * 0.02}px)`,
+        }}>
+        <img src={Cloud1Png} alt="Cloud PokeBall" />
+      </div>
+      <div
+        className={s.cloudBig}
+        style={{
+          transform: `translate(${screenX * 0.02}px, ${screenY * 0.02}px)`,
+        }}>
+        <img src={Cloud2Png} alt="Cloud Big PokeBall" />
+      </div>
+      <div
+        className={s.pokeBall}
+        style={{
+          transform: `translate(${screenX * 0.04}px, ${screenY * 0.04}px)`,
+        }}>
+        <img src={Poketball} alt="Big PokeBall" />
+      </div>
+      <div
+        className={s.pikachu}
+        style={{
+          transform: `translate(${screenX * 0.05}px, ${screenY * 0.05}px)`,
+        }}>
+        <img src={PikachuPng} alt="Pikachu" />
       </div>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,21 +6,26 @@ import s from './Home.module.scss';
 import Button from '../../components/Button';
 import Parallax from '../../components/Parallax';
 import Heading from '../../components/Heading';
+import Layout from '../../components/Layout';
 
 const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
-      <div>
-        <Heading type="h1">Find all your favorite Pokemon</Heading>
+      <Layout className={s.contentWrap}>
+        <div className={s.contentText}>
+          <Heading type="h1">Find all your favorite Pokemon</Heading>
 
-        <Heading type="h3">You can know the type of Pokemon, its strengths, disadvantages and abilities</Heading>
+          <Heading type="h3">You can know the type of Pokemon, its strengths, disadvantages and abilities</Heading>
 
-        <Button color="green" size="large">
-          Test
-        </Button>
-        <Parallax />
-      </div>
+          <Button color="green" size="large">
+            Test
+          </Button>
+        </div>
+        <div className={s.contentParallax}>
+          <Parallax />
+        </div>
+      </Layout>
     </div>
   );
 };
