d4d7eb1742a2a538620c2df9d41d56e477d5a224
Daniel <daniilterawars@gmail.com>
2021-05-17 08:25:16 +0300

    refactor: Heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,8 +4,7 @@ interface HeadingProps {
   type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 const Heading: React.FC<HeadingProps> = ({ type, children }) => {
-  const CustomTag: any = `${type}`;
-  return <CustomTag>{children}</CustomTag>;
+  return React.createElement(`${type}`, {}, children);
 };
 
 export default Heading;
